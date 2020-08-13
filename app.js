86f9c1e8e6ade50abe2bab38b5b862923e0ebefb
Nick Guerra <nickg@maliciousgames.com>
2020-08-13 06:44:15 -0500

    linked list blueprint added

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-function smth() {
+class LinkedList {
+    constructor() {
+        this.head = null; // first element of list
+        this.tail = null; // last element of list
+    }
 
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+
+        if (this.tail) {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+
+        if (!this.head) {
+            this.head = newNode;
+        }
+    }
+
+    toArray() {
+        const elements = [];
+
+        let curNode = this.tail;
+        while (curNode) {
+            elements.push(curNode);
+            curNode = curNode.next;
+        }
+        return elements;
+    }
 }
+
+const linkedList1 = new LinkedList();
+linkedList1.append(1);
+linkedList1.append("hello world");
+linkedList1.append(true);
+linkedList1.append(18.51);
+
+console.log(linkedList1.toArray());

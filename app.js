79ef8de4c556b93f9a83c4a1a57fba26d72f9557
Nick Guerra <nickg@maliciousgames.com>
2020-08-14 06:26:44 -0500

    finish linked list

--- app.js
+++ app.js
@@ -20,10 +20,75 @@ class LinkedList {
         }
     }
 
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: this.head
+        }
+
+        this.head = newNode;
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+    }
+
+    insertAfter(value, afterValue) {
+        const existingNode = this.find(afterValue);
+
+        if (existingNode) {
+            const newNode = {
+                value: value,
+                next: existingNode.next
+            }
+            existingNode.next = newNode;
+
+        }
+    }
+
+    find(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        let curNode = this.head;
+
+        while (curNode) {
+            if (curNode.value === value) {
+                return curNode;
+            }
+            curNode = curNode.next;
+        }
+        return null;
+    }
+
+    delete(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        while (this.head && this.head.value === value) {
+            this.head = this.head.next;
+        }
+
+        let curNode = this.head;
+
+        while (curNode.next) {
+            if (curNode.next.value === value) {
+                curNode.next = curNode.next.next;
+            } else {
+                curNode = curNode.next;
+            }
+        }
+
+        if (this.tail.value === value) {
+            this.tail = curNode;
+        }
+    }
+
     toArray() {
         const elements = [];
 
-        let curNode = this.tail;
+        let curNode = this.head;
         while (curNode) {
             elements.push(curNode);
             curNode = curNode.next;
@@ -35,7 +100,23 @@ class LinkedList {
 const linkedList1 = new LinkedList();
 linkedList1.append(1);
 linkedList1.append("hello world");
+linkedList1.append("hello world");
 linkedList1.append(true);
 linkedList1.append(18.51);
+linkedList1.append('hello there');
+linkedList1.prepend('First Value');
+linkedList1.prepend('First Value');
 
+console.log(linkedList1.toArray());
+
+linkedList1.delete("hello world");
+linkedList1.delete('First Value');
+linkedList1.delete(18.51);
+
+console.log(linkedList1.toArray());
+console.log(linkedList1.find('Max'));
+console.log(linkedList1.find(true));
+
+linkedList1.insertAfter('new value - 1', true);
+linkedList1.insertAfter('new value - 2', 'hello there');
 console.log(linkedList1.toArray());
